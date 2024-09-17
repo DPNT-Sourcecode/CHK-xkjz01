b5fbe49dc35d7b7109cd04466018abf95b96686f
sbx_user1051 <sbx_user1051@169.254.122.117>
2024-09-17 00:11:45 +0000

    Tue Sep 17 00:11:45 UTC 2024

--- src/test/java/befaster/solutions/SUM/SumSolutionTest.java
+++ src/test/java/befaster/solutions/SUM/SumSolutionTest.java
@@ -2,10 +2,9 @@ package befaster.solutions.SUM;
 
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.BeforeEach;
-
-
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.equalTo;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class SumSolutionTest {
     private SumSolution sum;
@@ -17,6 +16,12 @@ public class SumSolutionTest {
 
     @Test
     public void compute_sum() {
+        assertEquals(3, solution.sum(1,2));
+        assertEquals(7, solution.sum(3,4));
+        assertEquals(10, solution.sum(3,4));
+        assertEquals(10,solution.sum(6,4));
+        assertEquals(0, solution.sum(0,0));
         assertThat(sum.compute(1, 1), equalTo(2));
     }
 }
+
