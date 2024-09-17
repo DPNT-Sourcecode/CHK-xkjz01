9ebf6f349c14d9165a71a223e675ac20d52a61df
sbx_user1051 <sbx_user1051@169.254.122.117>
2024-09-17 01:11:10 +0000

    Tue Sep 17 01:11:10 UTC 2024

--- src/test/java/befaster/solutions/CHK/CheckoutSolutionTest.java
+++ src/test/java/befaster/solutions/CHK/CheckoutSolutionTest.java
@@ -6,49 +6,53 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class CheckoutSolutionTest {
     @Test
-    public void testBasicItems() {
-        // Testando itens básicos sem ofertas
-        assertEquals(50, CheckoutSolution.checkout("A"), "Checkout for A should be 50");
-        assertEquals(30, CheckoutSolution.checkout("B"), "Checkout for B should be 30");
-        assertEquals(20, CheckoutSolution.checkout("C"), "Checkout for C should be 20");
-        assertEquals(15, CheckoutSolution.checkout("D"), "Checkout for D should be 15");
+    public void testEmptyBasket() {
+        assertEquals(0, CheckoutSolution.checkout(""));
     }
 
     @Test
-    public void testMultipleItemsWithoutOffers() {
-        // Testando múltiplos itens sem ofertas
-        assertEquals(100, CheckoutSolution.checkout("AA"), "Checkout for AA should be 100");
-        assertEquals(60, CheckoutSolution.checkout("BB"), "Checkout for BB should be 60");
-        assertEquals(40, CheckoutSolution.checkout("CC"), "Checkout for CC should be 40");
+    public void testSingleItemWithoutOffer() {
+        assertEquals(50, CheckoutSolution.checkout("A"));
+        assertEquals(30, CheckoutSolution.checkout("B"));
+        assertEquals(20, CheckoutSolution.checkout("C"));
+        assertEquals(15, CheckoutSolution.checkout("D"));
     }
 
     @Test
-    public void testItemsWithOffers() {
-        // Testando itens com ofertas especiais
-        assertEquals(130, CheckoutSolution.checkout("AAA"), "Checkout for AAA with special offer should be 130");
-        assertEquals(175, CheckoutSolution.checkout("AAAB"), "Checkout for AAAB with special offer should be 175");
-        assertEquals(75, CheckoutSolution.checkout("AAB"), "Checkout for AAB with special offer should be 175");
+    public void testSingleItemWithOffer() {
+        // Testing the offer for 'A'
+        assertEquals(130, CheckoutSolution.checkout("AAA"));
+        assertEquals(50, CheckoutSolution.checkout("A"));  // Offer should not apply
+        assertEquals(180, CheckoutSolution.checkout("AAAA")); // 130 + 50 = 180
+
+        // Testing the offer for 'B'
+        assertEquals(45, CheckoutSolution.checkout("BB"));
+        assertEquals(30, CheckoutSolution.checkout("B"));  // Offer should not apply
+        assertEquals(75, CheckoutSolution.checkout("BBB")); // 45 + 30 = 75
     }
 
     @Test
-    public void testMixedItems() {
-        // Testando combinação de itens e ofertas
-        assertEquals(190, CheckoutSolution.checkout("AABCD"), "Checkout for AABCD should be 190");
-        assertEquals(195, CheckoutSolution.checkout("AAABBC"), "Checkout for AAABBC should be 195");
+    public void testMultipleItemsWithOffers() {
+        // Items and offers combined
+        assertEquals(175, CheckoutSolution.checkout("AAABBB")); // 130 + 45
+        assertEquals(195, CheckoutSolution.checkout("AAABBBCC")); // 130 + 45 + 20 = 195
+        assertEquals(195, CheckoutSolution.checkout("AAABBBCCC")); // Same as above, with extra C
+        assertEquals(250, CheckoutSolution.checkout("AAABBBCCCD")); // 130 + 45 + 20 + 15 = 210
     }
 
     @Test
     public void testInvalidItems() {
-        // Testando itens inválidos
-        assertEquals(-1, CheckoutSolution.checkout("AABX"), "Checkout for AABX should be -1");
-        assertEquals(-1, CheckoutSolution.checkout("123"), "Checkout for 123 should be -1");
-        assertEquals(-1, CheckoutSolution.checkout(""), "Checkout for empty string should be -1");
+        assertEquals(-1, CheckoutSolution.checkout("E"));
+        assertEquals(-1, CheckoutSolution.checkout("ABCDE"));
+        assertEquals(-1, CheckoutSolution.checkout("A1B2C3"));
     }
 
     @Test
-    public void testEmptyInput() {
-        // Testando entrada vazia
-        assertEquals(0, CheckoutSolution.checkout(""), "Checkout for empty basket should be 0");
+    public void testLargeQuantity() {
+        // Large quantity to test performance and correctness
+        assertEquals(19500, CheckoutSolution.checkout("A".repeat(390)));// 130 + 130 + ... + 130
+        assertEquals(13500, CheckoutSolution.checkout("B".repeat(300))); // 45 + 45 + ... + 45
     }
 }
+
 
